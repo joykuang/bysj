e14b811e59cfd73597752135028aee5255443cbe
Joy Kuang <q-cute@163.com>
2015-03-11 19:56:17 +0800

    新增 Public/fonts

--- default.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//require './lq/index.html';
-//require './studio/index.html';
-require './studio/wf.html';
-?>
--- test.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-?>
-[{status: 0, url: "/JFZR.jpg", message: "上传完成！" }]
