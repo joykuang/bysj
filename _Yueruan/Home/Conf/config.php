1338b8dab0008c73958da94a6e1ac54dc3b73892
Joy Kuang <q-cute@163.com>
2015-03-19 20:53:06 +0800

    修正冲突

--- _Yueruan/Home/Conf/config.php
+++ _Yueruan/Home/Conf/config.php
@@ -16,12 +16,12 @@ return array(
         //一旦关闭多模块访问后，就只能访问默认模块（这里设置的是Home）。单模块设计后公共模块依然有效
 	
         //数据库的相关配置
-        //'APP_DEBUG' =>true,
-        //'DB_TYPE'   =>'mysql',
-        //'DB_HOST'   =>'mysql.sql104.cdncenter.net',
-        //'DB_NAME'   =>'sq_joykuang',
-        //'DB_USER'   =>'sq_joykuang',
-        //'DB_PWD'    =>'sq_joykuang',
-        //'DB_PREFIX' =>'studio_',
-   
+        /* 'APP_DEBUG'     =>        true,
+        'DB_TYPE'           =>        'mysql',
+        'DB_HOST'          =>        'localhost',
+        'DB_NAME'         =>        'daocms',
+        'DB_USER'          =>        'root',
+        'DB_PWD'           =>        'joy',
+        'DB_PREFIX'       =>        'cms_',
+        */
 );
